983410298bd2b855cbdaba96819225f988cf8d72
mimose <mimosecy@gmail.com>
2021-03-28 15:51:37 +0800

    develop: perfect the TestCase

--- deduplicate-common/src/main/java/com/mimose/component/deduplicate/exceptions/DuplicateException.java
+++ deduplicate-common/src/main/java/com/mimose/component/deduplicate/exceptions/DuplicateException.java
@@ -1,11 +1,14 @@
 package com.mimose.component.deduplicate.exceptions;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author mimose
  * @description exception when there is a duplication
  * @date 2021/3/26
  */
 public final class DuplicateException extends Exception {
+    private static final String DUPLICATE_EX_DEFAULT_MESSAGE = "A duplicated exception occurred";
 
     public DuplicateException() {
         super();
@@ -17,5 +20,14 @@ public final class DuplicateException extends Exception {
 
     public DuplicateException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    @Override
+    public String getMessage() {
+        final String message = super.getMessage();
+        if(StringUtils.isEmpty(message)) {
+            return DUPLICATE_EX_DEFAULT_MESSAGE;
+        }
+        return message;
     }
 }
---
+++ deduplicate-test/src/main/java/com/mimose/component/deduplicate/test/TestErrorCase.java
@@ -0,0 +1,28 @@
+package com.mimose.component.deduplicate.test;
+
+import com.mimose.component.deduplicate.exceptions.DuplicateException;
+import com.mimose.component.deduplicate.proxy.Proxies;
+
+/**
+ * Copyright (C) 2020 广东百慧科技有限公司
+ *
+ * <p></p>
+ *
+ * @author ccy
+ * @version 0.0.1
+ * @since 1.8
+ */
+public class TestErrorCase {
+
+    public static void main(String[] args) {
+        try {
+            TestService service = Proxies.proxy(new TestService());
+            service.getOne("123");
+            System.out.println("TestService getOne, id=123");
+            service.getOne("123");
+            System.out.println("TestService getOne, id=123");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
